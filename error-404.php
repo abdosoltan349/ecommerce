b55d004035e59fef2c89251579b45b25c2d86ef2
Abdulrahman Mohamed <abdosoltan349@gmail.com>
2020-03-06 00:33:58 +0200

    set errors pages by abdosoltan

--- error-404.php
+++ error-404.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 
 <?php
- include "init.php";
-	?>
+ include "admin/init.php";
+?>
 <html class="loading" lang="en" data-textdirection="ltr">
 <!-- BEGIN: Head-->
 
@@ -13,9 +13,9 @@
     <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
     <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="Error 404">
-    <title>Error 500 - Vuexy - Bootstrap HTML admin template</title>
+    <title>Error 404 - Vuexy - Bootstrap HTML admin template</title>
     <link rel="apple-touch-icon" href="<?php echo $images ?>ico/apple-icon-120.png">
-    <link rel="shortcut icon" type="image/x-icon" href="<?php echo $images ?>ico/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="admin/<?php echo $images ?>ico/favicon.ico">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600" rel="stylesheet">
 
     <!-- BEGIN: Vendor CSS-->
@@ -23,21 +23,21 @@
     <!-- END: Vendor CSS-->
 
     <!-- BEGIN: Theme CSS-->
-    <link rel="stylesheet" type="text/css" href="<?php echo $css ?>bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo $css ?>bootstrap-extended.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo $css ?>colors.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo $css ?>components.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo $css ?>themes/dark-layout.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo $css ?>themes/semi-dark-layout.css">
+    <link rel="stylesheet" type="text/css" href="admin/<?php echo $css ?>bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="admin/<?php echo $css ?>bootstrap-extended.css">
+    <link rel="stylesheet" type="text/css" href="admin/<?php echo $css ?>colors.css">
+    <link rel="stylesheet" type="text/css" href="admin/<?php echo $css ?>components.css">
+    <link rel="stylesheet" type="text/css" href="admin/<?php echo $css ?>themes/dark-layout.css">
+    <link rel="stylesheet" type="text/css" href="admin/<?php echo $css ?>themes/semi-dark-layout.css">
 
     <!-- BEGIN: Page CSS-->
-    <link rel="stylesheet" type="text/css" href="<?php echo $css ?>core/menu/menu-types/vertical-menu.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo $css ?>core/colors/palette-gradient.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo $css ?>pages/error.css">
+    <link rel="stylesheet" type="text/css" href="admin/<?php echo $css ?>core/menu/menu-types/vertical-menu.css">
+    <link rel="stylesheet" type="text/css" href="admin/<?php echo $css ?>core/colors/palette-gradient.css">
+    <link rel="stylesheet" type="text/css" href="admin/<?php echo $css ?>pages/error.css">
     <!-- END: Page CSS-->
 
     <!-- BEGIN: Custom CSS-->
-    <link rel="stylesheet" type="text/css" href="<?php echo $css ?>style.css">
+    <link rel="stylesheet" type="text/css" href="admin/<?php echo $css ?>style.css">
     <!-- END: Custom CSS-->
 
 </head>
@@ -54,43 +54,49 @@
             <div class="content-header row">
             </div>
             <div class="content-body">
-                <!-- error 500 -->
+                <!-- error 404 -->
                 <section class="row flexbox-container">
                     <div class="col-xl-7 col-md-8 col-12 d-flex justify-content-center">
                         <div class="card auth-card bg-transparent shadow-none rounded-0 mb-0 w-100">
                             <div class="card-content">
                                 <div class="card-body text-center">
-                                    <img src="<?php echo $images ?>pages/500.png" class="img-fluid align-self-center" alt="branding logo">
-                                    <h1 class="font-large-2 mt-1 mb-0">Internal Server Error!</h1>
-                                    <p class="p-3">
-                                        susceptive nonturbinated indoctrination formulary dyskinetic deafforest Strumella frolicsomeness encrustment
-                                        portia myelination lachrymatory bestain hoople piscator pyramidoidal parter clipt.
+                                    <img src="admin/<?php echo $images ?>pages/404.png" class="img-fluid align-self-center" alt="branding logo">
+                                    <h1 class="font-large-2 my-1">404 - Page Not Found!</h1>
+                                    <p class="p-2">
+                                        paraphonic unassessable foramination Caulopteris worral Spirophyton encrimson esparcet aggerate chondrule
+                                        restate whistler shallopy biosystematy area bertram plotting unstarting quarterstaff.
                                     </p>
-                                    <a class="btn btn-primary btn-lg" href="index.php">Back to Home</a>
+                                    <a class="btn btn-primary btn-lg mt-2" href="index.php">Back to Home</a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </section>
-                <!-- error 500 end -->
+                <!-- error 404 end -->
 
             </div>
         </div>
     </div>
     <!-- END: Content-->
 
-
+<script>
+//Using setTimeout to execute a function after 5 seconds.
+setTimeout(function () {
+   //Redirect with JavaScript
+   window.location.href= 'index.php';
+}, 5000);
+</script>
     <!-- BEGIN: Vendor JS-->
-    <script src="<?php echo $vendors ?>js/vendors.min.js"></script>
+    <script src="admin/<?php echo $vendors ?>js/vendors.min.js"></script>
     <!-- BEGIN Vendor JS-->
 
     <!-- BEGIN: Page Vendor JS-->
     <!-- END: Page Vendor JS-->
 
     <!-- BEGIN: Theme JS-->
-    <script src="layout/js/core/app-menu.js"></script>
-    <script src="layout/js/core/app.js"></script>
-    <script src="layout/js/scripts/components.js"></script>
+    <script src="admin/layout/js/core/app-menu.js"></script>
+    <script src="admin/layout/js/core/app.js"></script>
+    <script src="admin/layout/js/scripts/components.js"></script>
     <!-- END: Theme JS-->
 
     <!-- BEGIN: Page JS-->
--- error-500.php
+++ error-500.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 
 <?php
- include "init.php";
+ include "admin/init.php";
 	?>
 <html class="loading" lang="en" data-textdirection="ltr">
 <!-- BEGIN: Head-->
@@ -13,31 +13,31 @@
     <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
     <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="Error 404">
-    <title>Error 404 - Vuexy - Bootstrap HTML admin template</title>
-    <link rel="apple-touch-icon" href="<?php echo $images ?>ico/apple-icon-120.png">
+    <title>Error 500 - Vuexy - Bootstrap HTML admin template</title>
+    <link rel="apple-touch-icon" href="admin/<?php echo $images ?>ico/apple-icon-120.png">
     <link rel="shortcut icon" type="image/x-icon" href="<?php echo $images ?>ico/favicon.ico">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600" rel="stylesheet">
 
     <!-- BEGIN: Vendor CSS-->
-    <link rel="stylesheet" type="text/css" href="<?php echo $vendors ?>css/vendors.min.css">
+    <link rel="stylesheet" type="text/css" href="admin/<?php echo $vendors ?>css/vendors.min.css">
     <!-- END: Vendor CSS-->
 
     <!-- BEGIN: Theme CSS-->
-    <link rel="stylesheet" type="text/css" href="<?php echo $css ?>bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo $css ?>bootstrap-extended.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo $css ?>colors.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo $css ?>components.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo $css ?>themes/dark-layout.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo $css ?>themes/semi-dark-layout.css">
+    <link rel="stylesheet" type="text/css" href="admin/<?php echo $css ?>bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="admin/<?php echo $css ?>bootstrap-extended.css">
+    <link rel="stylesheet" type="text/css" href="admin/<?php echo $css ?>colors.css">
+    <link rel="stylesheet" type="text/css" href="admin/<?php echo $css ?>components.css">
+    <link rel="stylesheet" type="text/css" href="admin/<?php echo $css ?>themes/dark-layout.css">
+    <link rel="stylesheet" type="text/css" href="admin/<?php echo $css ?>themes/semi-dark-layout.css">
 
     <!-- BEGIN: Page CSS-->
-    <link rel="stylesheet" type="text/css" href="<?php echo $css ?>core/menu/menu-types/vertical-menu.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo $css ?>core/colors/palette-gradient.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo $css ?>pages/error.css">
+    <link rel="stylesheet" type="text/css" href="admin/<?php echo $css ?>core/menu/menu-types/vertical-menu.css">
+    <link rel="stylesheet" type="text/css" href="admin/<?php echo $css ?>core/colors/palette-gradient.css">
+    <link rel="stylesheet" type="text/css" href="admin/<?php echo $css ?>pages/error.css">
     <!-- END: Page CSS-->
 
     <!-- BEGIN: Custom CSS-->
-    <link rel="stylesheet" type="text/css" href="<?php echo $css ?>style.css">
+    <link rel="stylesheet" type="text/css" href="admin/<?php echo $css ?>style.css">
     <!-- END: Custom CSS-->
 
 </head>
@@ -54,25 +54,25 @@
             <div class="content-header row">
             </div>
             <div class="content-body">
-                <!-- error 404 -->
+                <!-- error 500 -->
                 <section class="row flexbox-container">
                     <div class="col-xl-7 col-md-8 col-12 d-flex justify-content-center">
                         <div class="card auth-card bg-transparent shadow-none rounded-0 mb-0 w-100">
                             <div class="card-content">
                                 <div class="card-body text-center">
-                                    <img src="<?php echo $images ?>pages/404.png" class="img-fluid align-self-center" alt="branding logo">
-                                    <h1 class="font-large-2 my-1">404 - Page Not Found!</h1>
-                                    <p class="p-2">
-                                        paraphonic unassessable foramination Caulopteris worral Spirophyton encrimson esparcet aggerate chondrule
-                                        restate whistler shallopy biosystematy area bertram plotting unstarting quarterstaff.
+                                    <img src="admin/<?php echo $images ?>pages/500.png" class="img-fluid align-self-center" alt="branding logo">
+                                    <h1 class="font-large-2 mt-1 mb-0">Internal Server Error!</h1>
+                                    <p class="p-3">
+                                        susceptive nonturbinated indoctrination formulary dyskinetic deafforest Strumella frolicsomeness encrustment
+                                        portia myelination lachrymatory bestain hoople piscator pyramidoidal parter clipt.
                                     </p>
-                                    <a class="btn btn-primary btn-lg mt-2" href="index.php">Back to Home</a>
+                                    <a class="btn btn-primary btn-lg" href="index.php">Back to Home</a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </section>
-                <!-- error 404 end -->
+                <!-- error 500 end -->
 
             </div>
         </div>
@@ -81,16 +81,16 @@
 
 
     <!-- BEGIN: Vendor JS-->
-    <script src="<?php echo $vendors ?>js/vendors.min.js"></script>
+    <script src="admin/<?php echo $vendors ?>js/vendors.min.js"></script>
     <!-- BEGIN Vendor JS-->
 
     <!-- BEGIN: Page Vendor JS-->
     <!-- END: Page Vendor JS-->
 
     <!-- BEGIN: Theme JS-->
-    <script src="layout/js/core/app-menu.js"></script>
-    <script src="layout/js/core/app.js"></script>
-    <script src="layout/js/scripts/components.js"></script>
+    <script src="admin/layout/js/core/app-menu.js"></script>
+    <script src="admin/layout/js/core/app.js"></script>
+    <script src="admin/layout/js/scripts/components.js"></script>
     <!-- END: Theme JS-->
 
     <!-- BEGIN: Page JS-->
