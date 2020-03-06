4fac3a4397083fb581ffcde40ccaa7adb8c53759
Abdulrahman Mohamed <abdosoltan349@gmail.com>
2020-03-06 15:15:08 +0200

    fix Hot Item

--- includes/templates/jslink.php
+++ includes/templates/jslink.php
@@ -1,39 +1,19 @@
 
-	<!-- Jquery -->
-    <script src="<?php echo $js ?>jquery.min.js"></script>
-    <script src="<?php echo $js ?>jquery-migrate-3.0.0.js"></script>
-	<script src="<?php echo $js ?>jquery-ui.min.js"></script>
-	<!-- Popper JS -->
-	<script src="<?php echo $js ?>popper.min.js"></script>
-	<!-- Bootstrap JS -->
-	<script src="<?php echo $js ?>bootstrap.min.js"></script>
-	<!-- Color JS -->
-	<script src="<?php echo $js ?>colors.js"></script>
-	<!-- Slicknav JS -->
-	<script src="<?php echo $js ?>slicknav.min.js"></script>
-	<!-- Owl Carousel JS -->
-	<script src="<?php echo $js ?>owl-carousel.js"></script>
-	<!-- Magnific Popup JS -->
-	<script src="<?php echo $js ?>magnific-popup.js"></script>
-	<!-- Fancybox JS -->
-	<script src="<?php echo $js ?>facnybox.min.js"></script>
-	<!-- Waypoints JS -->
-	<script src="<?php echo $js ?>waypoints.min.js"></script>
-	<!-- Jquery Counterup JS -->
-	<script src="<?php echo $js ?>jquery-counterup.min.js"></script>
-	<!-- Countdown JS -->
-	<script src="<?php echo $js ?>finalcountdown.min.js"></script>
-	<!-- Nice Select JS -->
-	<script src="<?php echo $js ?>nicesellect.js"></script>
-	<!-- Ytplayer JS -->
-	<script src="<?php echo $js ?>ytplayer.min.js"></script>
-	<!-- Flex Slider JS -->
-	<script src="<?php echo $js ?>flex-slider.js"></script>
-	<!-- ScrollUp JS -->
-	<script src="<?php echo $js ?>scrollup.js"></script>
-	<!-- Onepage Nav JS -->
-	<script src="<?php echo $js ?>onepage-nav.min.js"></script>
-	<!-- Easing JS -->
-	<script src="<?php echo $js ?>easing.js"></script>
-	<!-- Active JS -->
-	<script src="<?php echo $js ?>active.js"></script>
+	
+    <script src="<?php echo $js ?>jquery.min.layout/js"></script>
+    <script src="<?php echo $js ?>jquery-migrate-3.0.0.layout/js"></script>
+	<script src="<?php echo $js ?>jquery-ui.min.layout/js"></script>
+	<script src="<?php echo $js ?>popper.min.layout/js"></script>
+	<script src="<?php echo $js ?>bootstrap.min.layout/js"></script>
+	<script src="<?php echo $js ?>colors.layout/js"></script>
+	<script src="<?php echo $js ?>slicknav.min.layout/js"></script>
+	<script src="<?php echo $js ?>owl-carousel.layout/js"></script>
+	<script src="<?php echo $js ?>magnific-popup.layout/js"></script>
+	<script src="<?php echo $js ?>waypoints.min.layout/js"></script>
+	<script src="<?php echo $js ?>finalcountdown.min.layout/js"></script>
+	<script src="<?php echo $js ?>nicesellect.layout/js"></script>
+	<script src="<?php echo $js ?>flex-slider.layout/js"></script>
+	<script src="<?php echo $js ?>scrollup.layout/js"></script>
+	<script src="<?php echo $js ?>onepage-nav.min.layout/js"></script>
+	<script src="<?php echo $js ?>easing.layout/js"></script>
+	<script src="<?php echo $js ?>active.layout/js"></script>
