db4094cdf040fadaf57b148a209e78ac8d87f4be
yasohero11 <220170047@fa-hists.edu.eg>
2020-03-07 16:24:38 +0200

    update index file

--- includes/templates/head.php
+++ includes/templates/head.php
@@ -6,16 +6,19 @@
 <link rel="icon" type="image/png" href="images/favicon.png">
 <link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">	
 <link rel="stylesheet" href="<?php echo $css?>bootstrap.css">
+<link rel="stylesheet" href="<?php echo $css?>magnific-popup.min.css">
 <link rel="stylesheet" href="<?php echo $css?>font-awesome.css">
+<link rel="stylesheet" href="<?php echo $css?>jquery.fancybox.min.css">
 <link rel="stylesheet" href="<?php echo $css?>themify-icons.css">
 <link rel="stylesheet" href="<?php echo $css?>niceselect.css">
-<link rel="stylesheet" href="<?php echo $css?>reset.css">
-<link rel="stylesheet" href="<?php echo $css?>style.css">
-<link rel="stylesheet" href="<?php echo $css?>responsive.css">
-<link rel="stylesheet" href="<?php echo $css?>magnific-popup.min.css">
-<link rel="stylesheet" href="<?php echo $css?>jquery.fancybox.min.css">
+
 <link rel="stylesheet" href="<?php echo $css?>animate.css">
+
 <link rel="stylesheet" href="<?php echo $css?>flex-slider.min.css">
 <link rel="stylesheet" href="<?php echo $css?>owl-carousel.css">
 <link rel="stylesheet" href="<?php echo $css?>slicknav.min.css">
-</head>
+<link rel="stylesheet" href="<?php echo $css?>reset.css">
+<link rel="stylesheet" href="<?php echo $css?>style.css">
+<link rel="stylesheet" href="<?php echo $css?>responsive.css">
+
+
--- includes/templates/jslink.php
+++ includes/templates/jslink.php
@@ -15,18 +15,12 @@
 	<script src="<?php echo $js ?>owl-carousel.js"></script>
 	<!-- Magnific Popup JS -->
 	<script src="<?php echo $js ?>magnific-popup.js"></script>
-	<!-- Fancybox JS -->
-	<script src="<?php echo $js ?>facnybox.min.js"></script>
 	<!-- Waypoints JS -->
 	<script src="<?php echo $js ?>waypoints.min.js"></script>
-	<!-- Jquery Counterup JS -->
-	<script src="<?php echo $js ?>jquery-counterup.min.js"></script>
 	<!-- Countdown JS -->
 	<script src="<?php echo $js ?>finalcountdown.min.js"></script>
 	<!-- Nice Select JS -->
 	<script src="<?php echo $js ?>nicesellect.js"></script>
-	<!-- Ytplayer JS -->
-	<script src="<?php echo $js ?>ytplayer.min.js"></script>
 	<!-- Flex Slider JS -->
 	<script src="<?php echo $js ?>flex-slider.js"></script>
 	<!-- ScrollUp JS -->
@@ -37,3 +31,43 @@
 	<script src="<?php echo $js ?>easing.js"></script>
 	<!-- Active JS -->
 	<script src="<?php echo $js ?>active.js"></script>
+	<?php 
+	/*
+	<script src="../../layout/js/jquery.min.js"></script>
+    <script src="../../layout/js/jquery-migrate-3.0.0.js"></script>
+	<script src="../../layout/js/jquery-ui.min.js"></script>
+	<!-- Popper JS -->
+	<script src="../../layout/js/popper.min.js"></script>
+	<!-- Bootstrap JS -->
+	<script src="../../layout/js/bootstrap.min.js"></script>
+	<!-- Color JS -->
+	<script src="../../layout/js/colors.js"></script>
+	<!-- Slicknav JS -->
+	<script src="../../layout/js/slicknav.min.js"></script>
+	<!-- Owl Carousel JS -->
+	<script src="../../layout/js/owl-carousel.js"></script>
+	<!-- Magnific Popup JS -->
+	<script src="../../layout/js/magnific-popup.js"></script>
+	<!-- Waypoints JS -->
+	<script src="../../layout/js/waypoints.min.js"></script>
+	<!-- Countdown JS -->
+	<script src="../../layout/js/finalcountdown.min.js"></script>
+	<!-- Nice Select JS -->
+	<script src="../../layout/js/nicesellect.js"></script>
+	<!-- Flex Slider JS -->
+	<script src="../../layout/js/flex-slider.js"></script>
+	<!-- ScrollUp JS -->
+	<script src="../../layout/js/scrollup.js"></script>
+	<!-- Onepage Nav JS -->
+	<script src="../../layout/js/onepage-nav.min.js"></script>
+	<!-- Easing JS -->
+	<script src="../../layout/js/easing.js"></script>
+	<!-- Active JS -->
+	<script src="../../layout/js/active.js"></script>
+	<!-- Fancybox JS -->
+	*/
+	
+	?>
+	
+	
+	
