6c35d228d8a78d26198624483b410589f9d23144
Abdulrahman Mohamed <abdosoltan349@gmail.com>
2020-03-07 17:05:32 +0200

    rename image to images

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -35,7 +35,7 @@
 					<div class="col-lg-2 col-md-2 col-12">
 						<!-- Logo -->
 						<div class="logo">
-							<a href="index.html"><img src="<?php echo $images?>logo.png" alt="logo"></a>
+							<a href="index"><img src="<?php echo $images?>logo.png" alt="logo"></a>
 						</div>
 						<!--/ End Logo -->
 						<!-- Search Form -->
