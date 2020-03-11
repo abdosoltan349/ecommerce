9234c566a1131ef46fdba310d89129bad8a94d6e
Abdulrahman Mohamed <abdosoltan349@gmail.com>
2020-03-11 12:02:06 +0200

    CH

---
+++ check.php
@@ -0,0 +1,15 @@
+/*<?php
+$file_pointer = "config.php";  
+if(file_exists("$file_pointer")){
+    if (!unlink($file_pointer)) {  
+        echo ("$file_pointer cannot be deleted due to an error");  
+    }  
+    else {  
+        echo ("$file_pointer has been deleted");  
+    }  
+      
+}
+else {
+    echo "hhhh";
+}
+?>*/
