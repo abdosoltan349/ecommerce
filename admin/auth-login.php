5493ced0925210871a94fb56a30c3549b57a44d6
Abdulrahman Mohamed <abdosoltan349@gmail.com>
2020-03-20 21:05:08 +0200

    login page updated

--- admin/auth-login.php
+++ admin/auth-login.php
@@ -125,22 +125,22 @@ if(isset($_SESSION["adminid"])){
                                                                 </div>
                                                             </fieldset>
                                                         </div>
-                                                        <div class="text-right"><a href="auth-forgot-password" class="card-link">Forgot Password?</a></div>
+                                                        <!--div class="text-right"><a href="auth-forgot-password" class="card-link">Forgot Password?</a></div-->
                                                     </div>
-                                                    <a href="auth-register" class="btn btn-outline-primary float-left btn-inline">Register</a>
+                                                    <!--a href="auth-register" class="btn btn-outline-primary float-left btn-inline">Register</a-->
                                                     <button type="submit" class="btn btn-primary float-right btn-inline">Login</button>
                                                 </form>
                                             </div>
                                         </div>
                                         <div class="login-footer">
                                             <div class="divider">
-                                                <div class="divider-text">OR</div>
+                                                <div class="divider-text"></div>
                                             </div>
                                             <div class="footer-btn d-inline">
-                                                <a href="#" class="btn btn-facebook"><span class="fa fa-facebook"></span></a>
+                                                <!--a href="#" class="btn btn-facebook"><span class="fa fa-facebook"></span></a>
                                                 <a href="#" class="btn btn-twitter white"><span class="fa fa-twitter"></span></a>
                                                 <a href="#" class="btn btn-google"><span class="fa fa-google"></span></a>
-                                                <a href="#" class="btn btn-github"><span class="fa fa-github-alt"></span></a>
+                                                <a href="#" class="btn btn-github"><span class="fa fa-github-alt"></span></a-->
                                             </div>
                                         </div>
                                     </div>
