a5da6d3c1f224e06942dcb259bfe625b49855149
Ahmed Azab <ahmedazab1112@gmail.com>
2020-03-23 14:29:28 +0200

    ahmedazab

---
+++ admin/includes/functions/tax.php
@@ -0,0 +1,59 @@
+<?php
+
+
+class tax {
+    
+        
+            
+    
+       function setTax($Tname,$Tpercentage){
+               include "init.php";
+	  
+        $stmt = $conn->prepare("INSERT INTO tax (name,percentage)
+							VALUES (:name,:percentage)");
+	   $stmt->execute(array(
+	
+            'name' => $Tname,
+            'percentage' => $Tpercentage,
+            
+	   ));
+       
+       
+       
+       }
+    
+   
+       function getTax(){
+               include "init.php";
+
+        
+       $stmt = $conn->prepare("SELECT taxid,name,percentage FROM tax");
+       $stmt->execute();
+       $admins = $stmt->fetchAll();
+       return $admins;
+        
+    }
+    
+    
+       function updateTax($taxid,$Tname,$Tpercentage){
+       include "init.php";
+	
+       $stmt = $conn->prepare("UPDATE tax SET name=?,percentage=? WHERE taxid=?");
+	   $stmt->execute(array($Tname,$Tpercentage,$taxid));
+         
+         
+    }
+    
+       function deleteTax($taxid){
+       include "init.php";
+	   $stmt =  $conn->prepare("DELETE FROM tax WHERE taxid = ? ");
+	   $stmt->execute(array($taxid));
+    }
+    
+    
+
+}
+
+
+
+  ?>
