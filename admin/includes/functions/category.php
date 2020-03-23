ce81bf6181e22abf3890532b50902ab2864f2ec2
HusseinSonbol <HusseinSonbol>
2020-03-23 16:43:12 +0200

    Sonbol CATE

---
+++ admin/includes/functions/category.php
@@ -0,0 +1,44 @@
+<?php
+
+class category{
+
+    
+    function setCategorie($categoriename){
+    include "init.php";
+           $stmt = $conn->prepare("INSERT INTO categorie(catename)
+							VALUES (:catename)");
+	   $stmt->execute(array(
+		'catename' =>  $categoriename        
+     ));
+    }
+    
+    function getCategorie(){
+    include "init.php";
+    $stmt = $conn->prepare("SELECT * from categorie  WHERE 1");
+    $stmt->execute();
+    $category = $stmt->fetchAll();
+    return $category;
+        
+        
+    }
+    
+    function updateCategorie($catename,$categorieid){
+    include "init.php";
+        
+      $stmt = $conn->prepare("UPDATE categorie SET catename=? WHERE categorieid=?");
+	   $stmt->execute(array($catename,$categorieid));
+        
+    }
+    
+    function deleteCategorie($categorieid){
+    include "init.php";
+        $stmt =  $conn->prepare("DELETE FROM categorie WHERE categorieid = ? ");
+	   $stmt->execute(array($categorieid));
+        
+        
+    }
+}
+
+
+
+?>
